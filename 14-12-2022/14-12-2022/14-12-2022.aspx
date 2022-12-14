6afa2c47263426194dc18f6f6f93e6f93f8bbbcb
rama-shararah <115452637+rama-shararah@users.noreply.github.com>
2022-12-14 23:53:46 +0300

    task

---
+++ 14-12-2022/14-12-2022/14-12-2022.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="14-12-2022.aspx.cs" Inherits="_14_12_2022._14_12_2022" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Name"></asp:Label>
+            &nbsp;
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Age"></asp:Label>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="City"></asp:Label>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label4" runat="server" Text="Phone"></asp:Label>
+            &nbsp;
+            <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label5" runat="server" Text="Image"></asp:Label>
+            &nbsp;
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" />
+
+            <br />
+            <br />
+            <br />
+            <br />
+
+            <asp:Label ID="Label6" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 14-12-2022/14-12-2022/14-12-2022.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace _14_12_2022
+{
+    public partial class _14_12_2022 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void GetData()
+        {
+            SqlConnection connec = new SqlConnection("data source = DESKTOP-KG1IER4\\SQLEXPRESS; database = 14-12-2022 ; integrated security=SSPI");
+
+            SqlCommand comand = new SqlCommand("select * from customers", connec);
+            connec.Open();
+
+            SqlDataReader sdr = comand.ExecuteReader();
+            string table = "<table class='table table-striped'> <tr><th>ID</th> <th>Name</th> <th>Age</th> <th>City</th> <th>Phone</th> <th>img_url</th>";
+            while (sdr.Read())
+            {
+                table += $"<tr><td>{sdr[0]}</td><td>{sdr[1]}</td><td>{sdr[2]}</td><td>{sdr[3]}</td><td>{sdr[4]}</td><td>{sdr[5]}</td></tr>";
+                Image image = new Image();
+                this.Controls.Add(image);
+                image.ImageUrl = (string)sdr[5];
+            }
+            table += "</table>";
+            Label6.Text = table;
+            connec.Close();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connect = new SqlConnection("data source = DESKTOP-KG1IER4\\SQLEXPRESS; database = 14-12-2022 ; integrated security=SSPI");
+            connect.Open();
+            string name = TextBox1.Text;
+            string age = TextBox2.Text;
+            string city = TextBox3.Text;
+            string phone = TextBox4.Text;
+
+            string folderPath = Server.MapPath("~/Files/");
+
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+
+            FileUpload1.SaveAs(folderPath + Path.GetFileName(FileUpload1.FileName));
+
+            string img = "~/Files/" + Path.GetFileName(FileUpload1.FileName);
+
+            string query = "insert into customers(NAME,AGE,CITY,PhoneNumber,IMG_URL)" + " values ('" + name + "','" + age + "','" + city + "','" + phone + "','" + img + "')";
+
+            SqlCommand command = new SqlCommand(query, connect);
+            command.ExecuteNonQuery();
+            connect.Close();
+
+            GetData();
+        }
+    }
+}
